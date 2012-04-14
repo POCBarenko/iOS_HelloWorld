9226bb9198be2d83968627fad2bec2b0a9f2a3f6
ios2602 <ios2602@Mac-mini-de-caelum.local>
2012-04-14 10:29:32 -0300

    Initial Commit

---
+++ HelloWorld/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  HelloWorld
+//
+//  Created by ios2602 on 12/04/14.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
